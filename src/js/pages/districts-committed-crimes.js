39053506c12635791fe540737d9bd3710cee2975
Rethaniel Ramos <rethanielramos@gmail.com>
2021-01-04 19:12:40 +0800

    add multi-select functionality to district-committed-crimes

--- src/js/pages/districts-committed-crimes.js
+++ src/js/pages/districts-committed-crimes.js
@@ -4,6 +4,16 @@ const queryTimeText = document.getElementById('query-time-text');
 const queryTimeVal = document.getElementById('query-time-val');
 const districtSelect = document.getElementById('district');
 const resetButton = document.getElementById('reset-btn');
+const committedCrimesForm = document.getElementById(
+  'form-avg-committed-crimes',
+);
+// MULTI SELECT
+const districtFiltersContainer = document.getElementById(
+  'district-filter-container',
+);
+const addDistrictBtn = document.getElementById('btn-add-district');
+const districtFiltersSet = new Set();
+
 const committedCrimesContainer = document.getElementById(
   'committed-crimes-table',
 );
@@ -60,14 +70,45 @@ committedCrimesTable.setData('/api/committed-crimes', {
   district: districtSelect.value,
 });
 
-districtSelect.onchange = e => {
+resetButton.onclick = () => {
+  districtFiltersSet.clear();
+  districtFiltersContainer.innerHTML = '';
+
   committedCrimesTable.setData(committedCrimesTable.getAjaxUrl(), {
-    district: districtSelect.value,
+    district: '',
   });
 };
 
-resetButton.onclick = e => {
+addDistrictBtn.onclick = () => {
+  const districtValue = districtSelect.value;
+
+  if (!districtFiltersSet.has(districtValue)) {
+    districtFiltersSet.add(districtValue);
+
+    // add district value as filter
+    const spanEl = document.createElement('span');
+    spanEl.classList.add('border', 'px-2', 'py-1', 'm-1');
+    spanEl.appendChild(document.createTextNode(districtValue));
+
+    // add 'X' button
+    const iEl = document.createElement('i');
+    iEl.classList.add('material-icons', 'align-top', 'ml-2', 'clear');
+    iEl.onclick = () => {
+      spanEl.remove();
+      districtFiltersSet.delete(districtValue);
+      committedCrimesTable.setData(committedCrimesTable.getAjaxUrl(), {
+        district: Array.from(districtFiltersSet),
+      });
+    };
+    iEl.innerHTML = 'clear';
+
+    spanEl.appendChild(iEl);
+
+    // add final span to filter container
+    districtFiltersContainer.appendChild(spanEl);
+  }
+
   committedCrimesTable.setData(committedCrimesTable.getAjaxUrl(), {
-    district: '',
+    district: Array.from(districtFiltersSet),
   });
 };
